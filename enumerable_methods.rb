3975f0670141383a541269055a795d937f14a581
ezeilo-su <ezeilo.su@gmail.com>
2020-04-24 17:02:35 +0100

    Complete my_select section

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -7,6 +7,26 @@ module Enumerable
     end
     self
   end
+
+  def my_each_with_index
+    return enum_for if block_given? == false
+
+    for i in 0...self.length
+      yield(self[i], i)
+    end
+    self
+  end
+
+  def my_select
+    return enum_for if block_given? == false
+
+    new_arr = []
+    my_each do |n|
+      new_arr << n if yield(n)
+    end
+    new_arr
+  end
 end
 
-[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].my_each { |x| puts "elm_#{x}" }
+even_num_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].my_select { |x| x % 2 == 0 }
+puts even_num_arr.to_s
