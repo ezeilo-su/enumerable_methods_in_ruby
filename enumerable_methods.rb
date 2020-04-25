7d5a3d6dc4a05a2f71a67d2238eb016905cd86e7
ezeilo-su <ezeilo.su@gmail.com>
2020-04-25 17:04:10 +0100

    Update usage in the main file

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -40,3 +40,21 @@ module Enumerable
     any
   end
 end
+
+# CODE USAGE GOES HERE!
+
+puts "\ntesting my_each method...\n"
+result = ({ firstName: 'Sunday', lastName: 'Ezeilo' }.my_each { |name, val| puts "#{name}: #{val}" })
+p result
+
+puts "\ntesting my_each_with_index method...\n"
+result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].my_each_with_index { |elm, ind| puts "index_#{ind}: #{elm}" }
+p result
+
+puts "\ntesting my_select method...\n"
+result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].my_select { |elm| elm % 2 == 0 }
+p result
+
+puts "\ntesting my_any? method...\n"
+result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].my_any? { |n| n < 1 }
+p result
